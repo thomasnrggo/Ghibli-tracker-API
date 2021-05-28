13f6c887afa1ca384f8c34b8650f61d033759df1
Luis Loaeza <luis.loaeza@hotmail.com>
2021-05-28 11:43:15 -0500

    Modified users model

--- users/models.py
+++ users/models.py
@@ -8,9 +8,7 @@ class Profile(models.Model):
     Proxy model that extends the base data with other information.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar_url = models.ImageField(upload_to='users/avatars',
-                                   blank=True,
-                                   null=True)
+    avatar_url = models.URLField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
