e61407bddc3dc20f3699acebed0091854b7fe06a
Luis Loaeza <luis.loaeza@hotmail.com>
2021-06-04 15:32:14 -0500

    added put method to ratings

--- ratings/api.py
+++ ratings/api.py
@@ -1,12 +1,28 @@
 from ratings.models import Ratings
 from ratings.serializers import RatingsSerializer
-from rest_framework import generics
+from rest_framework import generics, mixins
 from django_filters.rest_framework import DjangoFilterBackend
 
 
-class RatingsList(generics.ListCreateAPIView):
+class RatingsList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  generics.GenericAPIView):
     queryset = Ratings.objects.all()
     serializer_class = RatingsSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['user', 'movie']
     lookup_field = 'user'
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
